1d20e86efa465b50992463e8fb6dba60619f7b56
goashok <goashok@gmail.com>
2012-06-08 09:13:51 -0400

    starting work on yellow pages

---
+++ app/models/Address.java
@@ -0,0 +1,17 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import play.db.jpa.Model;
+
+@Entity
+@Table(name="tb_address")
+public class Address extends Model {
+
+	public String address1;
+	public String address2;
+	public String city;
+	public String state;
+	public String zip;
+}
---
+++ app/models/Business.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import play.data.validation.Phone;
+import play.db.jpa.Model;
+
+@Entity
+@Table(name="tb_business")
+public class Business extends Model{
+	
+	public String name;
+	@Phone
+	public String phone;
+	
+	@OneToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="tb_business_categories")
+	public List<Category> categories;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	public List<Address> addresses;
+	
+	@OneToMany(cascade=CascadeType.ALL)
+	public List<Hours> hours;
+	
+
+}
