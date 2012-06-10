37c26161ff393e6d395cd7773cf214ff525218ad
ashok shamnani <goashok@gmail.com>
2012-06-10 01:07:12 -0400

    added support for replies

---
+++ app/controllers/ReplyTo.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import models.EntityType;
+import models.Reply;
+import play.data.validation.Required;
+import play.mvc.*;
+
+@With({LocationController.class,Secure.class})
+public class ReplyTo extends Controller {
+
+	
+    public static void display(EntityType entityType, String postedBy, long entityId, String subject) {
+        render(entityType, postedBy, entityId,subject);
+    }
+    
+    public static void send(@Required(message="EntityType is required") EntityType entityType, @Required(message="postedBy is required") String postedBy, @Required(message="entityId is required") long entityId, @Required(message="Subject is required") String subject, @Required(message="Message cannot be empty")String message) {
+    	if(validation.hasErrors())
+        {
+    		params.flash("message");
+        	validation.keep();
+        	display(entityType, postedBy, entityId, subject);
+        }
+    	MailSender.replyTo(postedBy, subject, session.get("username"), message);
+    	flash.success("Your message has been sent");
+    	Classifieds.index();
+    }
+
+}
---
+++ app/models/Reply.java
@@ -0,0 +1,14 @@
+package models;
+
+import play.*;
+import java.util.*;
+
+public class Reply {
+	
+	public EntityType entityType;
+	public long entityId;
+	public String subject;
+	public long text;
+    
+	
+}
